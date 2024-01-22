2e3adfd0d91773869c62400a776b022016e158a4
Sawano <swannmerienne@gmail.com>
2024-01-22 18:42:55 +0100

    sql added

---
+++ tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE CAMPING (
+	NumCamping INTEGER NOT NULL,
+	NomCamping VARCHAR(50),
+ 	AddrCamping VARCHAR(50),
+ 	TelCamping VARCHAR(15),
+	DateOuv DATE,
+	DateFerm DATE,
+	PRIMARY KEY(NumCamping)
+);
+
+CREATE TABLE ACTIVITE (
+	NumActivite INTEGER NOT NULL,
+	NomActivite VARCHAR(50),
+	TypeActivite VARCHAR(50),
+	PRIMARY KEY(NumActivite)
+);
+
+CREATE TABLE ACTICAMPING (
+	NumCamping INTEGER NOT NULL,
+	NumActivite INTEGER NOT NULL,
+	PrixActivite FLOAT,
+	PRIMARY KEY(NumCamping,NumActivite),
+	FOREIGN KEY(NumCamping) REFERENCES CAMPING(NumCamping),
+	FOREIGN KEY(NumActivite) REFERENCES ACTIVITE(NumActivite)
+);
